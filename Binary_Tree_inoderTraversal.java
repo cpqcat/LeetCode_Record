e69401abe9a9c75223d78dab52460fa522f47e07
cpqcat <260515242@qq.com>
2020-08-26 23:12:39 +0800

    inoder

--- Binary_Tree_inoderTraversal.java
+++ Binary_Tree_inoderTraversal.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
 
 public class Binary_Tree_inoderTraversal {
-    List<Integer> inoderTraversal(TreeNode root) {
+    // 1. recursive
+    List<Integer> inoderTraversal_recursive(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         helper(root, res);
         return res;    
@@ -12,4 +15,33 @@ public class Binary_Tree_inoderTraversal {
         res.add(node.val);
         helper(node.right, res);
     }
+
+    // 2.iterative left children are stored to the stack
+    List<Integer> inorderTraversal_iterative_left(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+    }
+
+    // 3.iterative right children are stored to stack
+     
+    List<Integer> inorderTraversal_iterative_right(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.
+            }
+        }
+    }
+
 }
