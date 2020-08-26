02338c2b786c81bf486ca00e3a956c3498986979
cpqcat <260515242@qq.com>
2020-08-26 23:30:20 +0800

    inorder

--- Binary_Tree_inoderTraversal.java
+++ Binary_Tree_inoderTraversal.java
@@ -34,14 +34,31 @@ public class Binary_Tree_inoderTraversal {
     // 3.iterative right children are stored to stack
      
     List<Integer> inorderTraversal_iterative_right(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.
+        List<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            
+            TreeNode cur = stack.pop();
+            if (cur.right != null) stack.push(cur.right);
+            // only add to the result when left sub tree is null
+            if (cur.left == null) result.add(cur.val);
+            else {
+                // if left sub tree is no null, we need to
+                // add it back to the stack, but after its left sub tree.
+                // then set the left subtree to null.(next time we can
+                // add it)
+                stack.push(cur);
+                stack.push(cur.left);
+                cur.left = null;
+                cur.right = null;// we also need to cut the right subtree.
             }
+            
         }
+        
+        return result;
     }
 
 }
