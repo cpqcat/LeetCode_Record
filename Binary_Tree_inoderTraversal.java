acb07e36eb3f58b1434d4a6997d8ffbd01d786b5
cpqcat <260515242@qq.com>
2020-08-27 23:22:37 +0800

    inoder both

--- Binary_Tree_inoderTraversal.java
+++ Binary_Tree_inoderTraversal.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 
@@ -29,12 +30,14 @@ public class Binary_Tree_inoderTraversal {
             res.add(root.val);
             root = root.right;
         }
+        return res;
     }
 
-    // 3.iterative right children are stored to stack
+    // 3.iterative right children are stored to stack 
+    // may be not right
      
     List<Integer> inorderTraversal_iterative_right(TreeNode root) {
-        List<Integer> result = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
         if (root == null) return result;
         
         Stack<TreeNode> stack = new Stack<TreeNode>();
@@ -42,7 +45,7 @@ public class Binary_Tree_inoderTraversal {
         while (!stack.isEmpty()) {
             
             TreeNode cur = stack.pop();
-            if (cur.right != null) stack.push(cur.right);
+            if (cur.right != null) stack.push(cur.right); 
             // only add to the result when left sub tree is null
             if (cur.left == null) result.add(cur.val);
             else {
@@ -58,7 +61,38 @@ public class Binary_Tree_inoderTraversal {
             
         }
         
-        return result;
+        return result; 
     }
 
+    // 4. iterative both children are stored to stack, color flag
+    List<Integer> inorderTraversal_iteratvie_both(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        HashMap<TreeNode,Integer> map = new HashMap<>();
+        if (root == null) return res;
+        stack.push(root);
+        map.put(root, 1);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            if (1 == map.get(node)) {
+                if (node.right != null) {
+                    stack.push(node.right);
+                    map.put(node.right, 1);
+                }
+                
+                stack.push(node);
+                map.put(node, 2);
+
+                if (node.left != null) {
+                    stack.push(node.left);
+                    map.put(node.left, 1);
+                }
+            }
+            else {
+                res.add(node.val);
+            }
+        }
+        return res;
+    }
 }
