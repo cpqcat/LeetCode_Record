545efc6493c93de0093f0fdcdff6e8c31593f1f8
cpqcat <260515242@qq.com>
2020-09-24 23:21:25 +0800

    change

--- changecoins.java
+++ changecoins.java
@@ -4,6 +4,7 @@ class changecoins {
     public int coinChange(int[] coins, int amount) {
         int[] memo = new int[amount+1];
         Arrays.fill(memo, amount+1);
+        memo[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
                 if (i-coins[j] >=0)
