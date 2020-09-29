438d3fbe3a1e4f443d2ebdf820440829b7597466
cpqcat <260515242@qq.com>
2020-09-29 22:39:09 +0800

    improve

--- stock4.java
+++ stock4.java
@@ -11,4 +11,22 @@ class stock4 {
         }
         return dp[prices.length - 1][0];
     }
+
+        public int maxProfit1(int[] prices) {
+            int [][] dp = new int [prices.length][2];
+            if (dp == null || dp.length == 0) return 0; 
+            int preprofit0 = 0;
+            int profit0 = 0;
+            int profit1 = -prices[0];
+            
+            for (int i = 1; i < prices.length; i++) {
+                int tmp = profit0;
+                int nextprofit0 = Math.max(profit0,profit1 + prices[i]);
+                int nextprofit1 = Math.max(preprofit0 - prices[i], profit1);
+                preprofit0 = tmp;
+                profit0 = nextprofit0;
+                profit1 = nextprofit1;
+            }
+            return profit0;
+        }
 }
