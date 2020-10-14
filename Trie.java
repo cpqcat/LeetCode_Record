15e4039dda983a29bd04d7148379d71a73caca74
cpqcat <260515242@qq.com>
2020-10-14 22:42:55 +0800

    Trie

---
+++ Trie.java
@@ -0,0 +1,51 @@
+class Trie {
+    Trie[] next = new Trie[26];
+    boolean isEnd = false;
+    /** Initialize your data structure here. */
+    public Trie() {
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie curr = this;
+        for (char c : word.toCharArray()) {
+            if (curr.next[c - 'a'] == null) {
+                curr.next[c - 'a'] = new Trie();
+            }
+            curr = curr.next[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie curr = this;
+        for (char c : word.toCharArray()) {
+            if (curr.next[c - 'a'] == null) {
+                return false;
+            }
+            curr = curr.next[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Trie curr = this;
+        for (char c : prefix.toCharArray()) {
+            if (curr.next[c - 'a'] == null) {
+                return false;
+            }
+            curr = curr.next[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
